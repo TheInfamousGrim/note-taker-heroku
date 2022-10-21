2feba3e28afa10cbaf49614ab5d9ce4bd59b5e45
TheInfamousGrim <finchergeorge1@gmail.com>
2022-10-22 00:34:18 +0100

    Add timeout to delete note handler
    Added a timeout to allow the deleted note to
    be rendered off the screen.

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -93,7 +93,7 @@ const handleNoteDelete = (e) => {
     }
 
     deleteNote(noteId).then(() => {
-        getAndRenderNotes();
+        setTimeout(getAndRenderNotes, 500);
         renderActiveNote();
     });
 };
